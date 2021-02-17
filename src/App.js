26b803116da7431a5f94305cbd70474228b600b9
Roman Rangel <romanrangel106549@gmail.com>
2021-02-17 14:47:26 -0800

    added styling to webpage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-//===COMPONENT GOES HERE===
+// ===COMPONENT GOES HERE===
 function App() {
-  return (
-    <h3>= = Employee table goes here = =</h3>
-  );
+    return (
+        <table class="table text-center b-0">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+    );
 }
 
 export default App;
